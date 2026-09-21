810e5c58d2f71d52d98348bce498da9ddc24e44f
SevanthAG <sevanthrao1406@gmail.com>
2026-09-21 20:56:05 +0530

    first commit

---
+++ Introduction_To_Java/Print_First_5_Alphabets.java
@@ -0,0 +1,9 @@
+public class Print_First_5_Alphabets {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+    }
+}
