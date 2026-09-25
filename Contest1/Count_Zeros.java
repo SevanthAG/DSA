23616ec70d1512b16b74b88ee7f52542e19079fe
SevanthAG <sevanthrao1406@gmail.com>
2026-09-26 00:37:46 +0530

    25/09/20206

---
+++ Contest1/Count_Zeros.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Count_Zeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        sc.close();
+
+        int count = 0;
+
+        while (N != 0) {
+            if (N%10 == 0) {
+                count++;
+            }
+            N = N/10;
+        }
+        System.out.print(count);
+    }
+}
